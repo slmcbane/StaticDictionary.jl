e922a0b4f99026f2c18d2105f654e3704b2dce22
Sean McBane <seanmcb94@gmail.com>
2018-07-24 17:29:27 -0500

    Rewritten using @pure and type manipulation

--- src/ConstantDictionary.jl
+++ src/ConstantDictionary.jl
@@ -5,55 +5,51 @@ module ConstantDictionary
 export ConstDict, additem
 
 import Base: keys, values, getindex, haskey
+using Base: @pure
 
-struct ConstDict{KT, VT, K, V, C} 
-    @inline function ConstDict{KT, VT}(k::KT, v::VT) where KT where VT
-        new{KT, VT, k, v, Nothing}()
-    end
-    
-    @inline function ConstDict{KT, VT}(k::KT, v::VT, c::ConstDict) where KT where VT
-        @assert !haskey(c, k) "Key $k is already in keys of child dictionary"
-        new{KT, VT, k, v, typeof(c)}()
-    end
-    
-    @inline function ConstDict{KT, VT, K, V, C}() where {KT, VT, K, V} where C <: Union{ConstDict, Nothing}
-        new{KT, VT, K, V, C}()
-    end
+struct ConstDict{K, V, C}
+    @inline @pure ConstDict{K, V}() where {K, V} = new{K, V, Nothing}()
+    @inline @pure ConstDict{K, V}(::Type{C}) where {K, V} where C <: ConstDict = haskey(C, Val{K}) ? additem(C, Pair{Val{K}, Val{V}}) : new{K, V, C}()
+    @inline @pure ConstDict{K, V}(::C) where {K, V} where C <: ConstDict = ConstDict{K, V}(C)
+    @inline @pure ConstDict{K, V, Nothing}() where {K, V} = ConstDict{K, V}()
+    @inline @pure ConstDict{K, V, C}() where {K, V, C} = ConstDict{K, V}(C())
 end
 
-@inline ConstDict(k::K, v::V) where K where V = ConstDict{K, V}(k, v)
-@inline ConstDict(kv::Pair{K, V}) where K where V = ConstDict(kv[1], kv[2])
-@inline ConstDict(kv::Pair{K, V}, kvs::Pair...) where {K, V} = ConstDict{K, V}(kv[1], kv[2], ConstDict(kvs...))
+@inline @pure ConstDict(::Type{Val{K}}, ::Type{Val{V}}) where K where V = ConstDict{K, V}()
+@inline @pure ConstDict(::Type{Pair{Val{K}, Val{V}}}) where K where V = ConstDict{K, V}()
+@inline @pure ConstDict(::Type{Pair{Val{K}, Val{V}}}, kvs::DataType...) where {K, V} = ConstDict{K, V}(ConstDict(kvs...))
+@inline @pure ConstDict(k, v) = ConstDict(Val{k}, Val{v})
+@inline @pure ConstDict(kv::Pair) = ConstDict(Val{kv[1]}, Val{kv[2]})
+@inline @pure ConstDict(kv::Pair, kvs::Pair...) = additem(ConstDict(kvs...), kv)
 
-@inline keys(::ConstDict{KT, VT, K, V, Nothing}) where {KT, VT, K, V} = (K,)
-@inline keys(::ConstDict{KT, VT, K, V, C}) where {KT, VT, K, V} where C <: ConstDict = tuple_cat((K,), keys(C()))
+@inline @pure keys(::Type{ConstDict{K, V, Nothing}}) where {K, V} = (K,)
+@inline @pure keys(::Type{ConstDict{K, V, C}}) where {K, V} where C <: ConstDict = tuple_cat((K,), keys(C()))
+@inline @pure keys(::C) where C <: ConstDict = keys(C)
 
-@inline values(::ConstDict{KT, VT, K, V, Nothing}) where {KT, VT, K, V} = (V,)
-@inline values(::ConstDict{KT, VT, K, V, C}) where {KT, VT, K, V} where C <: ConstDict = tuple_cat((V,), values(C()))
+@inline @pure values(::Type{ConstDict{K, V, Nothing}}) where {K, V} = (V,)
+@inline @pure values(::Type{ConstDict{K, V, C}}) where {K, V} where C <: ConstDict = tuple_cat((V,), values(C()))
+@inline @pure values(::C) where C <: ConstDict = values(C)
 
-@inline function getindex(::ConstDict{KT, VT, K, V, Nothing}, k) where {KT, VT, K, V}
-    @assert k == K
-    V
-end
+@inline @pure getindex(::Type{ConstDict{K, V, Nothing}}, ::Type{Val{K}}) where {K, V} = V
+@inline @pure getindex(::Type{ConstDict{K, V, Nothing}}, ::Type{Val{N}}) where {K, V, N} = nothing
+@inline @pure getindex(::Type{ConstDict{K, V, C}}, ::Type{Val{K}}) where {K, V} where C <: ConstDict = V
+@inline @pure getindex(::Type{ConstDict{K, V, C}}, ::Type{Val{N}}) where {K, V, N} where C <: ConstDict = getindex(C, Val{N})
+@inline @pure getindex(::C, k) where C <: ConstDict = getindex(C, Val{k})
 
-@inline function getindex(::ConstDict{KT, VT, K, V, C}, k) where {KT, VT, K, V} where C <: ConstDict
-    k == K ? V : getindex(C(), k)
-end
+@inline @pure haskey(::Type{ConstDict{K, V, Nothing}}, ::Type{Val{K}}) where {K, V} = true
+@inline @pure haskey(::Type{ConstDict{K, V, Nothing}}, ::Type{Val{N}}) where {K, V, N} = false
+@inline @pure haskey(::Type{ConstDict{K, V, C}}, ::Type{Val{K}}) where {K, V} where C <: ConstDict = true
+@inline @pure haskey(::Type{ConstDict{K, V, C}}, ::Type{Val{N}}) where {K, V, N} where C <: ConstDict = haskey(C, Val{N})
+@inline @pure haskey(::C, k) where C <: ConstDict = haskey(C, Val{k})
 
-@inline function haskey(::ConstDict{KT, VT, K, V, C}, k) where {KT, VT, K, V} where C <: ConstDict
-    k == K || haskey(C(), k)
-end
+@inline @pure additem(::Type{ConstDict{K, V, Nothing}}, ::Type{Pair{Val{K}, Val{NV}}}) where {K, V, NV} = ConstDict{K, NV}()
+@inline @pure additem(::Type{ConstDict{K, V, Nothing}}, ::Type{Pair{Val{NK}, Val{NV}}}) where {K, V, NK, NV} = ConstDict{K, V}(ConstDict{NK, NV, Nothing})
+@inline @pure additem(::Type{ConstDict{K, V, C}}, ::Type{Pair{Val{K}, Val{NV}}}) where {K, V, NV} where C <: ConstDict = ConstDict{K, NV}(C)
+@inline @pure additem(::Type{ConstDict{K, V, C}}, ::Type{Pair{Val{NK}, Val{NV}}}) where {K, V, NK, NV} where C <: ConstDict = ConstDict{K, V}(additem(C, Pair{Val{NK}, Val{NV}}))
+@inline @pure additem(::C, kv::Pair) where C <: ConstDict = additem(C, Pair{Val{kv[1]}, Val{kv[2]}})
 
-@inline function haskey(::ConstDict{KT, VT, K, V, Nothing}, k) where {KT, VT, K, V}
-    k == K
-end
-
-@inline function additem(c::ConstDict{KT, VT, K, V}, kv::Pair{NK, NV}) where {KT, VT, K, V, NK, NV}
-    ConstDict{NK, NV}(kv[1], kv[2], c)
-end
-
-@inline tuple_cat(x::Tuple) = x
-@inline tuple_cat(x::Tuple, y::Tuple) = (x..., y...)
-@inline tuple_cat(x::Tuple, y::Tuple, zs::Tuple...) = (x..., tuple_cat(y, zs...)...)
+@inline @pure tuple_cat(x::Tuple) = x
+@inline @pure tuple_cat(x::Tuple, y::Tuple) = (x..., y...)
+@inline @pure tuple_cat(x::Tuple, y::Tuple, zs::Tuple...) = (x..., tuple_cat(y, zs...)...)
 
 end # module
